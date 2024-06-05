2fcb71ca95d172bed42873868e343eb106e1e6ed
Gareth Flynn <gareth.t.flynn@gmail.com>
2024-06-04 22:24:28 -0400

    functional image drag and drop working with form to create a new post

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import { FaPlus, FaAngleDown } from "react-icons/fa6";
 
 export default function Header() {
   return (
-    <nav class="flex items-center flex-wrap bg-[#110A02] p-4">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <a href="/homeFeed" class="font-semibold text-xl tracking-tight">
+    <nav className="flex items-center flex-wrap bg-[#ECE2D8] hover:bg-[#110A02] duration-1000 p-4">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <a href="/homeFeed" className="font-semibold text-xl tracking-tight">
           rcmnd
         </a>
         <input
@@ -14,23 +14,23 @@ export default function Header() {
           className="border border-[#ECE2D8] bg-transparent text-[#ECE2D8] px-2 py-1 my-1 ml-4 rounded hover:bg-[#513C2C] focus:within:bg-[#ECE2D8] outline-none placeholder-[#513C2C]"
         />
       </div>
-      <div class="w-full block flex-grow lg:flex lg:flex-end lg:items-center lg:w-auto">
-        <div class="w-full text-sm flex items-center justify-end">
+      <div className="w-full block flex-grow lg:flex lg:flex-end lg:items-center lg:w-auto">
+        <div className="w-full text-sm flex items-center justify-end">
           <a
             href="/homeFeed"
-            class="block mt-4 lg:inline-block lg:mt-0 text-[#FBF8F4] hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-[#FBF8F4] hover:text-white mr-4"
           >
             home
           </a>
           <a
             href="/profile"
-            class="block mt-4 lg:inline-block lg:mt-0 text-[#FBF8F4] hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-[#FBF8F4] hover:text-white mr-4"
           >
             profile
           </a>
           <a
             href="/notifications"
-            class="block mt-4 lg:inline-block lg:mt-0 text-[#FBF8F4] hover:text-white mr-2"
+            className="block mt-4 lg:inline-block lg:mt-0 text-[#FBF8F4] hover:text-white"
           >
             notifications
           </a>
@@ -42,7 +42,7 @@ export default function Header() {
           </a>
           <a
             href="/account"
-            class="lg:inline-block lg:mt-0 text-[#FBF8F4] hover:text-white "
+            className="lg:inline-block lg:mt-0 text-[#FBF8F4] hover:text-white "
           >
             <FaAngleDown style={{ color: "#FBF8F4", fontSize: "1.5em" }} />
           </a>
