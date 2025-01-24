ddc1784c9f6dbbae603639ec5482d016c8a9a22d
Gareth Flynn <gareth.t.flynn@gmail.com>
2025-01-24 09:36:02 -0600

    added nextauthURL to env var in settings

--- src/components/HeaderWithPathname.js
+++ src/components/HeaderWithPathname.js
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 
 export default function HeaderWithPathname() {
   const pathname = usePathname();
-  const shouldHideHeader = ["/sign-in", "/sign-up", "/welcome"].includes(pathname);
+  const shouldHideHeader = ["/sign-in", "/sign-up", "/welcome", "/"].includes(pathname);
   
   if (shouldHideHeader) return null;
 
