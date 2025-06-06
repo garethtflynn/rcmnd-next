b7e8d7df1568190877e5ef47191a9b677c3017e9
Gareth Flynn <gareth.t.flynn@gmail.com>
2025-06-06 08:20:10 -0500

    AppImage width fixed

--- src/components/PostItemDetailsPage.js
+++ src/components/PostItemDetailsPage.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 
 import AppImage from "./AppImage";
 
-
 export default function PostItemDetailsPage({
   id,
   title,
@@ -13,30 +12,28 @@ export default function PostItemDetailsPage({
   image,
   username,
   userId,
-  deletePostCallback,
 }) {
   return (
     <div
       id={id}
       className="h-screen w-full bg-[#000000] place-content-center place-items-center px-1 text-[#D7CDBF] md:grid md:grid-cols-2"
     >
-      <div className="w-2/3 md:w-1/2">
+      <div className='flex justify-center items-center w-9/12 sm:w-6/12 md:w-8/12'>
         <AppImage
-          className=""
           src={image}
           alt={title}
           // width="300"
           // height="300"
         />
       </div>
-      <div className="w-full h-fit  sm:px-4 py-4 md:w-3/4 md:px-0 md:pt-0 text-[#D7CDBF]">
+      <div className="w-full h-fit sm:px-4 py-4 md:w-3/4 md:px-0 md:pt-0 text-[#D7CDBF]">
         <div className="flex flex-row justify-between items-center">
           <Link href={`/user/${userId}`} className="hover:opacity-50">
             <h1 className="text-xl">@{username}</h1>
           </Link>
           <h1 className="text-xl py-4">NOTES</h1>
         </div>
-        <div className="border p-2 mx-1">
+        <div className="border mx-1 p-2">
           <p>{title}</p>
           <br />
           <Link href={link} target="_blank">
