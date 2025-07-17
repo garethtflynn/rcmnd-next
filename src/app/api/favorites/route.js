ae01019821e283e8df576c039feb14c364f2a803
Gareth Flynn <gareth.t.flynn@gmail.com>
2025-07-17 15:20:39 -0500

    really fixing the import this time

--- src/app/api/favorites/route.js
+++ src/app/api/favorites/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { prisma } from '@/libs/prisma';
+import prisma from "@/libs/db";
 
 export async function GET(request) {
   try {
