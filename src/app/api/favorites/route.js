636764fd500d72632c796a8e74da01b299cc731a
Gareth Flynn <gareth.t.flynn@gmail.com>
2025-07-17 15:17:43 -0500

    fixed import in route

--- src/app/api/favorites/route.js
+++ src/app/api/favorites/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { prisma } from '@/lib/prisma';
+import { prisma } from '@/libs/prisma';
 
 export async function GET(request) {
   try {
